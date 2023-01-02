d00a16e8296783debe7b25f898b20dd5d5d471b7
Simon <simon.sand3r@gmail.com>
2023-01-02 19:35:47 +0100

    Added title and mainTitle

--- CollimationCircles/ViewModels/BaseViewModel.cs
+++ CollimationCircles/ViewModels/BaseViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.Text.Json.Serialization;
 
 namespace CollimationCircles.ViewModels
 {
     public partial class BaseViewModel : ObservableValidator
-    {        
+    {
+        [ObservableProperty]
+        public string mainTitle = string.Empty;
+
+        [ObservableProperty]
+        public string title = string.Empty;
     }
 }
