00485f744ea13ee5f48fb4654f09dfc881cf1992
Simon <simon.sand3r@gmail.com>
2023-06-27 09:25:20 +0200

    Etit publish.cmd commands to be linux compatible

--- publish.cmd
+++ publish.cmd
@@ -1,11 +1,11 @@
 :: win-x64
-dotnet restore .\CollimationCircles.sln -r win-x64
-dotnet publish -c Release -f net7.0 -r win-x64 -o D:\Projekti\Publish\cc\win-64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
+dotnet restore ./CollimationCircles.sln -r win-x64
+dotnet publish -c Release -f net7.0 -r win-x64 -o ~/Projects/Publish/cc/win-64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
 
 :: linux-x64
 ::dotnet restore .\CollimationCircles.sln -r linux-x64
-::dotnet publish -c Release -f net7.0 -r linux-x64 -o D:\Projekti\Publish\cc\linux-64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
+::dotnet publish -c Release -f net7.0 -r linux-x64 -o ~/Projects/Publish/cc/linux-64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
 
 :: linux-arm64
 ::dotnet restore .\CollimationCircles.sln -r linux-arm64
-::dotnet publish -c Release -f net7.0 -r linux-arm64 -o D:\Projekti\Publish\cc\linux-arm64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
+::dotnet publish -c Release -f net7.0 -r linux-arm64 -o ~/Projects/Publish/cc/linux-arm64 --self-contained true /p:PublishSingleFile=true /p:PublishReadyToRun=true
