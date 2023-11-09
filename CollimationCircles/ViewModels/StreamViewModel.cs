089abc8a181198a67fb3526c6f103588ee77ef51
Simon <simon.sand3r@gmail.com>
2023-11-09 22:37:50 +0100

    Added ability to connect to camera stream and display it in background

--- CollimationCircles/ViewModels/StreamViewModel.cs
+++ CollimationCircles/ViewModels/StreamViewModel.cs
@@ -1,24 +1,32 @@
-﻿using Avalonia.Threading;
+﻿using System;
+using System.Diagnostics;
+using Avalonia.Threading;
 using CollimationCircles.Helper;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using HanumanInstitute.MvvmDialogs;
 using LibVLCSharp.Shared;
-using System;
-using System.Diagnostics;
-using System.Text;
 
 namespace CollimationCircles.ViewModels
 {
     public partial class StreamViewModel : BaseViewModel
     {
+        const string defaultLocalAddress = "127.0.0.1";
+        const string defaultRemoteAddress = "192.168.1.129";
+        const string defaultPort = "8080";
+
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         private readonly IDialogService dialogService;
         private readonly LibVLC libVLC;
         private Process? proc;
+        
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(PlayPauseCommand))]
+        private string address;
 
         [ObservableProperty]
         [NotifyCanExecuteChangedFor(nameof(PlayPauseCommand))]
-        private string mediaUri = "tcp/h264://192.168.1.129:8080";
+        private string port = defaultPort;
 
         public MediaPlayer MediaPlayer { get; }
 
@@ -26,63 +34,70 @@ namespace CollimationCircles.ViewModels
         private string buttonTitle = string.Empty;
 
         [ObservableProperty]
-        private string mediaPlayerLog = string.Empty;
+        private bool isRemoteConnection = !OperatingSystem.IsLinux();
 
-        private readonly StringBuilder _mediaPlayerLog = new();
-
-        private bool CanExecutePlayPause
+        public bool CanExecutePlayPause
         {
-            get => !string.IsNullOrWhiteSpace(MediaUri);
+            get => !string.IsNullOrWhiteSpace(Address) && !string.IsNullOrWhiteSpace(Port);
         }
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsEnabled))]
+        private bool isPlaying = false;
+
+        public bool IsEnabled => !IsPlaying && IsRemoteConnection;        
 
         public StreamViewModel(IDialogService dialogService)
         {
             this.dialogService = dialogService;
 
-            if (OperatingSystem.IsLinux())
-            {
-                MediaUri = "tcp/h264://127.0.0.1:8080";
-            }
+            Address = GetUrl();
 
             libVLC = new();
             //libVLC.Log += LibVLC_Log;
 
             MediaPlayer = new(libVLC);
+            MediaPlayer.Opening += MediaPlayer_Opening;
+            MediaPlayer.Playing += MediaPlayer_Playing;
+            MediaPlayer.Stopped += MediaPlayer_Stopped;                     
 
-            ButtonTitle = DynRes.TryGetString("Start");
+            ButtonTitle = DynRes.TryGetString("Start");            
         }
 
         private void LibVLC_Log(object? sender, LogEventArgs e)
         {
-            Dispatcher.UIThread.Post(() =>
-            {
-                _mediaPlayerLog.AppendLine(e.Message);
-                MediaPlayerLog = _mediaPlayerLog.ToString();
-            });
+            logger.Info(e.Message);
         }
 
         private void Play()
         {
-            if (!string.IsNullOrWhiteSpace(MediaUri))
+            if (!string.IsNullOrWhiteSpace(Address))
             {
-                if (OperatingSystem.IsLinux())
+                if (!IsRemoteConnection)
                 {
-                    Console.WriteLine("Starting video stream: libcamera-vid -t 0 --inline --nopreview --listen -o tcp://0.0.0.0:8080");
-
-                    ProcessStartInfo startInfo = new()
+                    try
                     {
-                        FileName = "libcamera-vid",
-                        Arguments = "-t 0 --inline --nopreview --listen -o tcp://0.0.0.0:8080"
-                    };
+                        ProcessStartInfo startInfo = new()
+                        {
+                            FileName = "libcamera-vid",
+                            Arguments = $"-t 0 --inline --nopreview --listen -o tcp://0.0.0.0:{Port}"
+                        };
 
-                    proc = new()
-                    {
-                        StartInfo = startInfo
-                    };
+                        proc = new()
+                        {
+                            StartInfo = startInfo
+                        };
 
-                    if (proc.Start())
+                        if (proc.Start())
+                        {
+                            logger.Info($"Started LibCamera process '{startInfo.FileName} {startInfo.Arguments}'");
+                            MediaPlayerPlay();
+                        }
+                    }
+                    catch (Exception ex)
                     {
-                        MediaPlayerPlay();
+                        logger.Info("Local connection is only supported on Linux.\nPlease use 'Connect remote' option.");
+                        logger.Error(ex.Message);
                     }
                 }
                 else
@@ -94,31 +109,41 @@ namespace CollimationCircles.ViewModels
 
         private void MediaPlayerPlay()
         {
+            string mrl = $"tcp/h264://{Address}:{Port}";
+
             using var media = new Media(
                     libVLC,
-                    MediaUri,
+                    mrl,
                     FromType.FromLocation
                     );
 
-            MediaPlayer.Opening += MediaPlayer_Opening;
-            MediaPlayer.Stopped += MediaPlayer_Stopped;
-
             MediaPlayer.Play(media);
-        }
-
-        private void MediaPlayer_Stopped(object? sender, EventArgs e)
-        {
-            CloseWebCamStream();
-            ButtonTitle = DynRes.TryGetString("Start");
+            logger.Info($"Playing web camera stream: '{media.Mrl}'");
         }
 
         private void MediaPlayer_Opening(object? sender, EventArgs e)
         {
+            logger.Info($"MediaPlayer opening");
+
             ShowWebCamStream();
             ButtonTitle = DynRes.TryGetString("Stop");
-            _mediaPlayerLog.Clear();
-            MediaPlayerLog = _mediaPlayerLog.ToString();
+            IsPlaying = MediaPlayer.IsPlaying;
         }
+
+        private void MediaPlayer_Playing(object? sender, EventArgs e)
+        {
+            logger.Info($"MediaPlayer playing");
+            IsPlaying = MediaPlayer.IsPlaying;
+        }
+
+        private void MediaPlayer_Stopped(object? sender, EventArgs e)
+        {
+            logger.Info($"MediaPlayer stopped");
+
+            CloseWebCamStream();
+            ButtonTitle = DynRes.TryGetString("Start");
+            IsPlaying = MediaPlayer.IsPlaying;
+        }        
 
         [RelayCommand(CanExecute = nameof(CanExecutePlayPause))]
         private void PlayPause()
@@ -141,6 +166,7 @@ namespace CollimationCircles.ViewModels
             Dispatcher.UIThread.Post(() =>
             {
                 dialogService?.Show<StreamViewModel>(null, this);
+                logger.Info($"Opened web camera stream window");
             });
         }
 
@@ -149,8 +175,28 @@ namespace CollimationCircles.ViewModels
             Dispatcher.UIThread.Post(() =>
             {
                 dialogService?.Close(this);
+                logger.Info($"Closed web camera stream window");
+
                 proc?.Close();
+                logger.Info($"Closed LibCamera process");
             });
         }
+
+        partial void OnIsRemoteConnectionChanged(bool oldValue, bool newValue)
+        {
+            string oldMediaUri = Address;
+
+            if (newValue) Address = defaultRemoteAddress;
+
+            Address = GetUrl();
+            Debug.WriteLine($"MediaUrl changed from '{oldMediaUri}:{Port}' to '{Address}:{Port}'");
+        }
+
+        private string GetUrl()
+        {
+            string newRemoteAddress = Address ?? defaultRemoteAddress;
+
+            return IsRemoteConnection ? newRemoteAddress : defaultLocalAddress;
+        }        
     }
 }
