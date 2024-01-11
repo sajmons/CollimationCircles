f5f71d651d9431bd18b612a49c32fe5b11b9f5b0
Simon <simon.sand3r@gmail.com>
2024-01-11 22:18:15 +0100

    OpenUrl method moved to AppService class

--- CollimationCircles/ViewModels/BaseViewModel.cs
+++ CollimationCircles/ViewModels/BaseViewModel.cs
@@ -1,56 +1,20 @@
 ﻿using Avalonia;
 using Avalonia.Markup.Xaml.Styling;
-using CollimationCircles.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 namespace CollimationCircles.ViewModels
 {
     public partial class BaseViewModel : ObservableValidator
     {
-        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-
         [ObservableProperty]
         public string title = string.Empty;
 
         [ObservableProperty]
-        public string mainTitle = string.Empty;
-
-        public static void OpenUrl(string url)
-        {
-            logger.Info($"Opening external url '{url}'");
-
-            try
-            {
-                Process.Start(url);
-            }
-            catch
-            {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&");
-                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    Process.Start("open", url);
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
+        public string mainTitle = string.Empty;        
 
         [RelayCommand]
         public static void Translate(string targetLanguage)
