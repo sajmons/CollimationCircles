76df9e5ab0abb7c5f09aca6aed0355d5f00ba684
Simon <simon.sand3r@gmail.com>
2024-03-26 12:01:00 +0100

    Camera control class

---
+++ CollimationCircles/Models/CameraControl.cs
@@ -0,0 +1,13 @@
+﻿namespace CollimationCircles.Models
+{
+    internal class CameraControl
+    {
+        public string Name { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Step { get; set; }
+        public int Default { get; set; }
+        public int Value { get; set; }
+        public string Flags { get; set; }
+    }
+}
