b67e7dceca9a67070200851fd136d657fd0add70
Simon <simon.sand3r@gmail.com>
2024-04-25 22:03:52 +0200

    Initialize SelectedItem

--- CollimationCircles/ViewModels/SettingsViewModel.cs
+++ CollimationCircles/ViewModels/SettingsViewModel.cs
@@ -85,7 +85,7 @@ namespace CollimationCircles.ViewModels
         private ObservableCollection<Color> colorList = [];
 
         [ObservableProperty]
-        private CollimationHelper? selectedItem;
+        private CollimationHelper? selectedItem = new();
 
         [JsonProperty]
         [ObservableProperty]
