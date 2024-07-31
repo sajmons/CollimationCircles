2812b181ed3bc43d62d5c48b9319e1dc45cde77a
Simon <simon.sand3r@gmail.com>
2024-07-31 12:56:15 +0200

    Removed unused parameter

--- CollimationCircles/Models/CameraControl.cs
+++ CollimationCircles/Models/CameraControl.cs
@@ -33,7 +33,7 @@ namespace CollimationCircles.Models
 
             try
             {
-                Initialize(controlName);
+                Initialize();
             }
             finally
             {
@@ -41,7 +41,7 @@ namespace CollimationCircles.Models
             }
         }
 
-        private void Initialize(ControlType controlName)
+        private void Initialize()
         {
             Value = Default;
         }
@@ -56,7 +56,7 @@ namespace CollimationCircles.Models
 
         public void SetDefault()
         {
-            Initialize(Name);
+            Initialize();
         }
     }
 }
