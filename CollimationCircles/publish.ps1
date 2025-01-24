668be9074c10cde6cd00fe85d622445cb19dfe05
Simon Šander <simon.sander@vizija.si>
2025-01-24 09:06:17 +0100

    Updated publish script

--- CollimationCircles/publish.ps1
+++ CollimationCircles/publish.ps1
@@ -217,7 +217,7 @@ Function PublishOne
         Remove-Item –path $Output/$Runtime/icon.icns
     }
 
-    GrantExecutablePermissions $appName $Output $Runtime
+    # GrantExecutablePermissions $appName $Output $Runtime
 
     # To maintain backward compatibility for downloading new version GitHub release files must be ordered so that win-x64 is the first file.
     # That's why I aded number infront of file name to maintain correct order.
