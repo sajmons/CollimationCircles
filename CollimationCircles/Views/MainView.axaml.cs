0e128bc087f39b9300a69716783b3eea4c629e5f
Simon <simon.sand3r@gmail.com>
2025-01-30 11:17:15 +0100

    Added more rom from the corner for HUD text

--- CollimationCircles/Views/MainView.axaml.cs
+++ CollimationCircles/Views/MainView.axaml.cs
@@ -76,12 +76,12 @@ namespace CollimationCircles.Views
                         shortcutsString += $"{sk.Key}: {sk.Value}{Environment.NewLine}";
                     }
 
-                    dhs.DrawText(context, shortcutsString, new Point(5, 5), Brushes.Yellow, vm.LabelSize);
+                    dhs.DrawText(context, shortcutsString, new Point(15, 10), Brushes.Yellow, vm.LabelSize);
                 }
 
                 if (!vm.LicenseService.IsValid)
                 {
-                    dhs.DrawText(context, vm.ResSvc.TryGetString("UnlicensedVersion"), new Point(5, vm.MainWindowHeight - 25), Brushes.Red, vm.LabelSize);
+                    dhs.DrawText(context, vm.ResSvc.TryGetString("UnlicensedVersion"), new Point(15, vm.MainWindowHeight - 35), Brushes.Red, vm.LabelSize);
                 }
 
                 if (items is not null)
